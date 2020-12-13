ee1ffd95b928de69e3169612f4c893b3b43de242
hborkows <hubert.borkowski@hotmail.com>
2020-12-13 12:40:18 +0100

    Added meta function call to main

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from apriori.apriori import apriori_ar
+from utils.taxonomy import Taxonomy
+from utils.item import Item
+from typing import Callable, Set
+
+
+def get_association_rules(_algorithm: Callable, _dataset: Set, _taxonomy: Taxonomy,
+                          min_interest: float = 2, min_sup: float = 3, min_conf: float = 0.6):
+    return _algorithm(dataset, taxonomy, min_interest, min_sup, min_conf)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    algorithms = [apriori_ar]
+    dataset = set()
+    taxonomy = Taxonomy()
+
+    for algorithm in algorithms:
+        result = get_association_rules(_algorithm=algorithm, _dataset=dataset, _taxonomy=taxonomy)
