3a3e0d52ea0267b6b959e0d65bfacc527e7a918b
hborkows <hubert.borkowski@hotmail.com>
2021-01-08 15:24:22 +0100

    Added cumulate function declaration

---
+++ cumulate/cumulate.py
@@ -0,0 +1,7 @@
+from utils.alg_utils import get_frequent_1_set, generate_hash_tree, generate_k_subsets, is_prefix
+from utils.taxonomy import Taxonomy
+from typing import List
+
+
+def cumulate_ar(transactions: List, taxonomy: Taxonomy, min_interest: float, min_sup: float, min_conf: float):
+    pass
